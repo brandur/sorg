ae48624af6f1a3e7b96a2f9c530b1b757e29725c
Brandur <brandur@mutelight.org>
2016-07-12 09:59:18 -0700

    Working parallel asset fetch (possibly racy?)

--- assets/assets.go
+++ assets/assets.go
@@ -10,29 +10,56 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
+// Number of simultaneous asset fetches that we should perform.
+const numWorkers = 5
+
 // Asset represents a site asset that should be fetch from a local URL and
 // stored as a local file.
 type Asset struct {
 	URL    string
 	Target string
+
+	// Err is set if a problem occurred while fetching the asset.
+	Err error
 }
 
 // Fetch performs an HTTP fetch for each given asset and stores them to their
 // corresponding local targets.
-func Fetch(assets []Asset) error {
-	client := &http.Client{}
+func Fetch(assets []*Asset) error {
+	var err error
+	var numWorked int
+	assetsChan := make(chan *Asset, len(assets))
+	doneChan := make(chan *Asset, numWorkers)
+
+	for i := 0; i < numWorkers; i++ {
+		go workAssets(assetsChan, doneChan)
+	}
 
 	for _, asset := range assets {
-		err := fetchAsset(client, asset)
-		if err != nil {
-			return err
+		assetsChan <- asset
+	}
+
+	for {
+		select {
+		case asset := <-doneChan:
+			numWorked++
+
+			// Quit when we're done or after encountering the first error.
+			if numWorked == len(assets) || asset.Err != nil {
+				err = asset.Err
+				goto done
+			}
 		}
 	}
 
-	return nil
+done:
+	// Signal workers to stop working.
+	close(assetsChan)
+
+	return err
 }
 
-func fetchAsset(client *http.Client, asset Asset) error {
+func fetchAsset(client *http.Client, asset *Asset) error {
 	if _, err := os.Stat(asset.Target); !os.IsNotExist(err) {
 		log.Debugf("Skipping asset because local file exists: %v", asset.URL)
 		return nil
@@ -68,4 +95,17 @@ func fetchAsset(client *http.Client, asset Asset) error {
 	}
 
 	return nil
+}
+
+func workAssets(assetsChan chan *Asset, doneChan chan *Asset) {
+	client := &http.Client{}
+
+	// Note that this loop falls through when the channel is closed.
+	for asset := range assetsChan {
+		err := fetchAsset(client, asset)
+		if err != nil {
+			asset.Err = err
+		}
+		doneChan <- asset
+	}
 }
