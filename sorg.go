495c505e131f8e851844e3beebc6566a6d00686b
Brandur <brandur@mutelight.org>
2016-08-01 00:20:31 -0700

    Fix fragments index header

--- sorg.go
+++ sorg.go
@@ -10,7 +10,7 @@ import (
 const (
 	// Release is the asset version of the site. Bump when any assets are
 	// updated to blow away any browser caches.
-	Release = "6"
+	Release = "7"
 )
 
 const (
