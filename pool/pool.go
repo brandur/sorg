7a015d7863c0acb8f11f5355e8f5a41a55faeeab
Brandur <brandur@mutelight.org>
2016-08-19 13:31:51 -0700

    Comment rewordings and move `close` statement

--- pool/pool.go
+++ pool/pool.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 )
 
-// Task encapsulates a work item that should go in the pool.
+// Task encapsulates a work item that should go in a work pool.
 type Task struct {
 	// Err holds an error that occurred during a task. Its result is only
 	// meaningful after Run has been called for the pool that holds it.
@@ -14,7 +14,7 @@ type Task struct {
 	f func() error
 }
 
-// NewTask initializes a new Task based on a given work function.
+// NewTask initializes a new task based on a given work function.
 func NewTask(f func() error) *Task {
 	return &Task{f: f}
 }
@@ -25,7 +25,7 @@ func (t *Task) Run(wg *sync.WaitGroup) {
 	wg.Done()
 }
 
-// Pool is a worker group that runs a number of Task instances at a configured
+// Pool is a worker group that runs a number of tasks at a configured
 // concurrency.
 type Pool struct {
 	Tasks []*Task
@@ -35,7 +35,7 @@ type Pool struct {
 	wg          *sync.WaitGroup
 }
 
-// NewPool initializes a new Pool with the given tasks and at the given
+// NewPool initializes a new pool with the given tasks and at the given
 // concurrency.
 func NewPool(tasks []*Task, concurrency int) *Pool {
 	return &Pool{
@@ -57,8 +57,7 @@ func (p *Pool) HasErrors() bool {
 	return false
 }
 
-// Run runs all work within the Pool. The first error that's detected after all
-// work is done is returned.
+// Run runs all work within the pool and blocks until it's finished.
 func (p *Pool) Run() {
 	log.Debugf("Running %v task(s) at concurrency %v.",
 		len(p.Tasks), p.concurrency)
@@ -71,12 +70,14 @@ func (p *Pool) Run() {
 	for _, task := range p.Tasks {
 		p.tasksChan <- task
 	}
-	p.wg.Wait()
 
 	// all workers return
 	close(p.tasksChan)
+
+	p.wg.Wait()
 }
 
+// The work loop for any single goroutine.
 func (p *Pool) work() {
 	for task := range p.tasksChan {
 		task.Run(p.wg)
