3012bd76f04103b7155716f8df85e09f359dd652
Brandur <brandur@mutelight.org>
2016-08-19 11:12:33 -0700

    Output multiple errors from a task queue run
    If multiple errors occurred while running a task queue, print up to 10
    of them. In the same style as the Go compiler, print "too many errors"
    if more than that occurred.
    
    
    Former-commit-id: 413cc49735eae2b7594897061e134c6b529536bc

--- pool/pool.go
+++ pool/pool.go
@@ -7,8 +7,11 @@ import (
 
 // Task encapsulates a work item that should go in the pool.
 type Task struct {
-	err error
-	f   func() error
+	// Err holds an error that occurred during a task. Its result is only
+	// meaningful after Run has been called for the pool that holds it.
+	Err error
+
+	f func() error
 }
 
 // NewTask initializes a new Task based on a given work function.
@@ -18,15 +21,16 @@ func NewTask(f func() error) *Task {
 
 // Run runs a Task and does appropriate accounting via a given sync.WorkGroup.
 func (t *Task) Run(wg *sync.WaitGroup) {
-	t.err = t.f()
+	t.Err = t.f()
 	wg.Done()
 }
 
 // Pool is a worker group that runs a number of Task instances at a configured
 // concurrency.
 type Pool struct {
+	Tasks []*Task
+
 	concurrency int
-	tasks       []*Task
 	tasksChan   chan *Task
 	wg          *sync.WaitGroup
 }
@@ -35,39 +39,42 @@ type Pool struct {
 // concurrency.
 func NewPool(tasks []*Task, concurrency int) *Pool {
 	return &Pool{
+		Tasks:       tasks,
 		concurrency: concurrency,
-		tasks:       tasks,
 		tasksChan:   make(chan *Task),
 		wg:          new(sync.WaitGroup),
 	}
 }
 
+// HasErrors indicates whether there were any errors from tasks run. Its result
+// is only meaningful after Run has been called.
+func (p *Pool) HasErrors() bool {
+	for _, task := range p.Tasks {
+		if task.Err != nil {
+			return true
+		}
+	}
+	return false
+}
+
 // Run runs all work within the Pool. The first error that's detected after all
 // work is done is returned.
-func (p *Pool) Run() error {
+func (p *Pool) Run() {
 	log.Debugf("Running %v task(s) at concurrency %v.",
-		len(p.tasks), p.concurrency)
+		len(p.Tasks), p.concurrency)
 
 	for i := 0; i < p.concurrency; i++ {
 		go p.work()
 	}
 
-	p.wg.Add(len(p.tasks))
-	for _, task := range p.tasks {
+	p.wg.Add(len(p.Tasks))
+	for _, task := range p.Tasks {
 		p.tasksChan <- task
 	}
 	p.wg.Wait()
 
 	// all workers return
 	close(p.tasksChan)
-
-	for _, task := range p.tasks {
-		if task.err != nil {
-			return task.err
-		}
-	}
-
-	return nil
 }
 
 func (p *Pool) work() {
