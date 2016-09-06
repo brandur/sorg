f04730be0865ed4de2347661efd7aeef33f812b4
Brandur <brandur@mutelight.org>
2016-09-06 12:46:19 -0700

    Drop JQuery + update Highcharts
    Drops the project's JQuery dependency. This mostly involves removing the
    trivial use of a `$(document).ready` handler and upgrading Highcharts
    which previously relied on JQuery being present.
    
    The Highcharts upgrade introduces some new styling for the charts
    (mostly more "flat"), but also some other nice features like better
    tooltips and better responsiveness for smaller displays (the Twitter
    page especially benefits from this last change).
    
    See the pull request for some screenshots in before and after chart
    differences.

--- content/javascripts/main.js
+++ content/javascripts/main.js
@@ -1,11 +1,17 @@
 //
-// Note: not currently used with main site. Only with the static build.
+// Main: all custom scripting should go here.
 //
 
-$(document).ready(function () {
-    hljs.initHighlightingOnLoad();
-});
+// The equivalent of JQuery's `$(document).ready`. It's a little newer, but
+// caniusethis estimates a 97+% penetration rate which is good enough for me.
+window.addEventListener('DOMContentLoaded', function () {
+  hljs.initHighlightingOnLoad();
+})
 
+// Highlight.js wants to be in `DOMContentLoaded` (and not `load`) while
+// Retina.js wants the opposite. I don't quite understand why this is, but I'm
+// cargo culting for now.
 window.addEventListener('load', function () {
-    retinajs();
+  // With no arguments looks for anything with a `data-rjs` tag.
+  retinajs();
 });
