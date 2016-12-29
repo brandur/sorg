bc6fb9d56d9807e8b9c6c56250207387f1e53940
Brandur <brandur@mutelight.org>
2016-12-29 00:02:25 -0700

    Remove me.jpg from retina check now that it has a @2x
    Former-commit-id: 27939c2aadb4b5f65e12177580fdc637e527a47e

--- scripts/check_retina.sh
+++ scripts/check_retina.sh
@@ -37,7 +37,6 @@ allowed_exceptions=(
     "./content/images/fragments/wgt-2015/vista.jpg"
     "./content/images/fragments/wgt-2015-brain-dump/jo-quail.jpg"
     "./content/images/fragments/wgt-2015-brain-dump/leaf.jpg"
-    "./content/images/me.jpg"
     "./content/images/page/economist.jpg"
     "./content/images/page/fp.png"
     "./content/images/page/rane.jpg"
