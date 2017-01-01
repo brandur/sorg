824871150b179f38edac8dfd955a7ea5637de2e7
Brandur <brandur@mutelight.org>
2017-01-01 14:56:12 -0700

    Introduce "overflowing" images for now and about pages
    Former-commit-id: 50aa8aab1aed0b8c3377a261b509d9afd91f6190

--- sorg.go
+++ sorg.go
@@ -11,7 +11,7 @@ import (
 const (
 	// Release is the asset version of the site. Bump when any assets are
 	// updated to blow away any browser caches.
-	Release = "17"
+	Release = "18"
 )
 
 const (
