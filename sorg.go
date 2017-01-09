672d29e60e55f13b9c6efde34bc0bec773c40739
Brandur <brandur@mutelight.org>
2017-01-09 08:47:42 -0700

    Bump assets version
    Former-commit-id: 02dd3c106ab74792c36381605312da3491acd51c

--- sorg.go
+++ sorg.go
@@ -11,7 +11,7 @@ import (
 const (
 	// Release is the asset version of the site. Bump when any assets are
 	// updated to blow away any browser caches.
-	Release = "19"
+	Release = "20"
 )
 
 const (
