9940844a01dea7d51fb352ff438d1f5bf01dea2e
Brandur <brandur@brandur.org>
2020-03-11 16:58:49 -0700

    Add test for without MozJPEG

--- build_test.go
+++ build_test.go
@@ -16,3 +16,22 @@ func TestResizeImage(t *testing.T) {
 	err = resizeImage(nil, "./content/images/about/avatar.jpg", tmpfile.Name(), 100)
 	assert.NoError(t, err)
 }
+
+func TestResizeImage_NoMozJPEG(t *testing.T) {
+	if conf.MozJPEGBin == "" {
+		return
+	}
+
+	oldMozJPEGBin := conf.MozJPEGBin
+	defer func() {
+		conf.MozJPEGBin = oldMozJPEGBin
+	}()
+	conf.MozJPEGBin = ""
+
+	tmpfile, err := ioutil.TempFile("", "resized_image")
+	assert.NoError(t, err)
+	defer os.Remove(tmpfile.Name())
+
+	err = resizeImage(nil, "./content/images/about/avatar.jpg", tmpfile.Name(), 100)
+	assert.NoError(t, err)
+}
