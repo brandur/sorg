f5f6219f31b61c6f18e60e3e2dc37e39e526615f
Brandur <brandur@brandur.org>
2022-12-28 22:18:45 -0700

    Add nav bar to atoms and sequences

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
+                'inter': ['Inter var', 'Inter', ...defaultTheme.fontFamily.sans],
                 // 'sans': ['Inter var', 'Inter', ...defaultTheme.fontFamily.sans],
                 'serif': ['Cardo', ...defaultTheme.fontFamily.sans],
             },
