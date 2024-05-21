eaf98d60b828d3a998465852e995efec7496366f
Brandur <brandur@brandur.org>
2024-05-21 09:09:06 +0200

    Implement dark mode and toggle
    Retheme the site so that it can support dark mode on the majority of
    active pages. Use a tri-state configuration so that along with explicit
    light and dark mode, respecting the user's preference at the OS level is
    not only possible, but default.
    
    Add an atom to note the change, with a follow up piece to be posted
    later.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-const defaultTheme = require( 'tailwindcss/defaultTheme' )
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -11,8 +12,16 @@ module.exports = {
         "./views/_*.ace",
         "./views/**/*.{html,js}"
     ],
+    darkMode: 'selector',
     theme: {
         extend: {
+            colors: {
+                proseBody: '#374151', // --tw-prose-body
+                proseLinks: '#111827', // --tw-prose-links
+                proseInvertBody: '#d1d5db', // --tw-prose-invert-body
+                proseInvertLinks: '#fff', // --tw-prose-invert-links
+                // darkBorder: colors["border-slate-800"],
+            },
             fontFamily: {
             },
         }
